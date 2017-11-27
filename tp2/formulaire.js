2c2d761326f09ccbb079ea1f3073e4bde9aaf4c6
AminAbid <32834201+AminAbid@users.noreply.github.com>
2017-11-27 15:20:37 +0100

    Add files via upload

---
+++ tp2/formulaire.js
@@ -0,0 +1,25 @@
+function validation() {
+    
+    if(
+    
+    document.getElementById("nom").value.length < 5 ||
+    document.getElementById("prenom").value.length < 5 ||
+    document.getElementById("date").value.length < 10 ||
+    document.getElementById("adresse").value.length < 5 ||
+    document.getElementById("mail").value.length < 5
+      )
+    
+    {
+    document.getElementById("resultat").setAttribute("hidden","hidden");
+    document.getElementById("error").removeAttribute("hidden");
+	document.getElementById("error").innerHTML = "La saisie de l'ensemble des champs obligatoire";    
+    }
+    
+    else
+    {
+	document.getElementById("error").setAttribute("hidden","hidden");
+    document.getElementById("resultat").removeAttribute("hidden");
+	document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#prenom").value+" "+ document.querySelector("#nom").value;    
+    }
+    
+						}
